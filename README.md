1aab561202d368e89502076839289910d3394d27
Mardyyy <martinapopot@gmail.com>
2023-06-18 23:40:35 -0500

    Added inquirer prompt and fs.writeFile for the README.md

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Martin-09-Node-README-Generator
-command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer package.
+
+    # 111111
+
+    ## Description
+    2222222222
+
+    ## Table of Contents (Optional)
+
+    If your README is long, add a table of contents to make it easy for users to find what they need.
+    
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+    ## Installation
+
+    undefined
+
+    ## Usage
+
+    4444444444
+
+    ## Credits
+
+    55555555555
+
+    ## License
+
+    6666666
+
+    ## Badges
+
+    ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+    Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+    ## Tests
+
+    777777777777
+    
+    ## Questions
+
+    For any additional questions, reach me at martinapopot@gmail.com or https://github.com/Mardyyy.
+
+    
--- index.js
+++ index.js
@@ -1,13 +1,121 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
+
 
 // Function call to initialize app
 init();
+
+// ------------
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'What is the description?',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'What are the instructions for use?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'What/who are the credits?',
+            name: 'credits',
+        },
+        {
+            type: 'input',
+            message: 'What license do you want?',
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'What are the tests for your application?',
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: 'What is your Github username?',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+        },
+    ])
+    .then((response) => {
+        fs.writeFile('README.md',
+    `
+    # ${response.title}
+
+    ## Description
+    ${response.description}
+
+    ## Table of Contents (Optional)
+
+    If your README is long, add a table of contents to make it easy for users to find what they need.
+    
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+    ## Installation
+
+    ${response.instructions}
+
+    ## Usage
+
+    ${response.usage}
+
+    ## Credits
+
+    ${response.credits}
+
+    ## License
+
+    ${response.license}
+
+    ## Badges
+
+    ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+    Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+    ## Tests
+
+    ${response.tests}
+    
+    ## Questions
+
+    For any additional questions, reach me at ${response.email} or https://github.com/${response.username}.
+
+    `
+    , (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+    });
